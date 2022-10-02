2d0e24bc6abd179575df34ca7993271561c25bee
Kyuwoong Lee <kwooler@gmail.com>
2022-10-02 15:24:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-numbers = [     [10, 11, 12, 13, 14],
-               [20, 21, 22, 23, 24],
-               [30, 31, 32, 33, 34]]
-
-# numbers =[] 
-# rnum = int(input('Enter the number of rows'))
-# for i in range(rnum):
-# 	row = list(map(int, input('Enter 5 values for a row: ').split()))
-# 	numbers.append(row)
+numbers = [    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
+               [1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+               [0, 1, 0, 0, 1, 1, 1, 0, 1, 0],
+               [0, 1, 0, 0, 0, 1, 0, 1, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+               [0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+               [0, 1, 1, 1, 0, 1, 0, 0, 1, 0],
+               [0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
+               [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]]
 
 rnum = len(numbers)
 cnum = len(numbers[0])
